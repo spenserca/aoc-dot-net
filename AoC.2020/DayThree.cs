583bbd195ee24d6c024db785648cf91498f31afc
Spenser Andreassen <756316@corp.caseys.com>
2021-11-18 14:16:59 -0600

    day three part one complete

---
+++ AoC.2020/DayThree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AoC._2020
+{
+    public class DayThree : IDay
+    {
+        public string Title => "--- Day 3: Toboggan Trajectory ---";
+
+        public object PartOne(string[] input)
+        {
+            var x = 3;
+            var xSlope = 3;
+            var y = 0;
+            var ySlope = 1;
+            var treeCount = 0;
+
+            // start on second line
+            for (int i = 1; i < input.Length; i++)
+            {
+                var line = input[i];
+                var location = line[x];
+                var tree = '#';
+
+                if (location == tree)
+                {
+                    treeCount++;
+                }
+
+                x = MoveHorizontally(x, line.Length);
+            }
+
+            return treeCount;
+        }
+
+        private static int MoveHorizontally(int currentLocation, int wrapPoint)
+        {
+            currentLocation += 3;
+            if (currentLocation >= wrapPoint)
+            {
+                currentLocation -= wrapPoint;
+            }
+
+            return currentLocation;
+        }
+
+        public object PartTwo(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AoC.UnitTests/AoC.2020/DayThreeTests.cs
@@ -0,0 +1,49 @@
+﻿using AoC._2020;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2020
+{
+    public class DayThreeTests
+    {
+        private readonly DayThree _underTest;
+
+        public DayThreeTests()
+        {
+            _underTest = new DayThree();
+        }
+
+        [Fact(DisplayName = "day three part one counts all the trees encountered on the way down")]
+        public void DayThreePartOne_TestOne()
+        {
+            var input = new[]
+            {
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#",
+            };
+
+            var actual = _underTest.PartOne(input);
+
+            actual.Should().Be(7);
+        }
+        
+        [Fact(DisplayName = "day three part one with puzzle input gets the correct answer")]
+        public void DayThreePartOne_TestTwo()
+        {
+            var input = FileReader.ReadAllLines(@"AoC.2020/Data/DayThree.txt");
+
+            var actual = _underTest.PartOne(input);
+
+            actual.Should().Be(636);
+        }
+    }
+}
