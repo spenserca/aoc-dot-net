e3d34d1548edba01e79ed7bd4f92ce13fb03fccb
Spenser Andreassen <756316@corp.caseys.com>
2021-12-14 15:32:47 -0600

    2021 - day three part one complete

---
+++ AoC.2021/DayThree.cs
@@ -0,0 +1,63 @@
+﻿using AoC.Common;
+
+namespace AoC._2021
+{
+    public class DayThree : IDay
+    {
+        public string Title => "--- Day 3: Binary Diagnostic ---";
+
+        public object PartOne(string[] input)
+        {
+            var bitCounters = GetBitCounts(input);
+
+            var epsilonRate = "";
+            var gammaRate = "";
+            foreach (var (index, bits) in bitCounters)
+            {
+                var sumOfZeroes = bits.Count(b => b == 0);
+                var sumOfOnes = bits.Count(b => b == 1);
+
+                if (sumOfZeroes > sumOfOnes)
+                {
+                    gammaRate += "0";
+                    epsilonRate += "1";
+                }
+                else
+                {
+                    gammaRate += "1";
+                    epsilonRate += "0";
+                }
+            }
+
+            return Convert.ToInt32(epsilonRate, 2) * Convert.ToInt32(gammaRate, 2);
+        }
+
+        private static Dictionary<int, List<int>> GetBitCounts(string[] input)
+        {
+            var bitCounters = new Dictionary<int, List<int>>();
+
+            foreach (var powerConsumption in input)
+            {
+                for (int j = 0; j < powerConsumption.Length; j++)
+                {
+                    var bitValue = Convert.ToInt32(powerConsumption[j].ToString());
+                    if (bitCounters.ContainsKey(j))
+                    {
+                        bitCounters[j].Add(bitValue);
+                    }
+                    else
+                    {
+                        bitCounters[j] = new List<int>() { bitValue };
+                    }
+                }
+            }
+
+            return bitCounters;
+        }
+
+        public object PartTwo(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AoC.UnitTests/AoC.2021/DayThreeTests.cs
@@ -0,0 +1,84 @@
+﻿using AoC._2021;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2021
+{
+    public class DayThreeTests
+    {
+        private readonly DayThree _underTest;
+
+        public DayThreeTests()
+        {
+            _underTest = new DayThree();
+        }
+
+        [Fact(DisplayName = "day three part one calculates the power consumption of the submarine")]
+        public void DayThreePartOneTest_One()
+        {
+            var input = new[]
+            {
+                "00100",
+                "11110",
+                "10110",
+                "10111",
+                "10101",
+                "01111",
+                "00111",
+                "11100",
+                "10000",
+                "11001",
+                "00010",
+                "01010"
+            };
+
+            var actual = _underTest.PartOne(input);
+
+            actual.Should().Be(198);
+        }
+
+        [Fact(DisplayName = "day three part one with puzzle input gets the correct answer")]
+        public void DayThreePartOneTest_Two()
+        {
+            var input = FileReader.ReadAllLines(@"AoC.2021/Data/DayThree.txt");
+        
+            var actual = _underTest.PartOne(input);
+        
+            actual.Should().Be(2595824);
+        }
+        
+        // [Fact(DisplayName = "day three part two calculates the life support rating")]
+        // public void DayThreePartTwoTest_One()
+        // {
+        //     var input = new[]
+        //     {
+        //         "00100",
+        //         "11110",
+        //         "10110",
+        //         "10111",
+        //         "10101",
+        //         "01111",
+        //         "00111",
+        //         "11100",
+        //         "10000",
+        //         "11001",
+        //         "00010",
+        //         "01010"
+        //     };
+        //
+        //     var actual = _underTest.PartTwo(input);
+        //
+        //     actual.Should().Be(230);
+        // }
+
+        // [Fact(DisplayName = "day three part two with puzzle input gets the correct answer")]
+        // public void DayThreePartTwoTest_Two()
+        // {
+        //     var input = FileReader.ReadAllLines(@"AoC.2021/Data/DayThree.txt");
+        //
+        //     var actual = _underTest.PartTwo(input);
+        //
+        //     actual.Should().Be(2006917119); // 2595824 - too high
+        // }
+    }
+}
