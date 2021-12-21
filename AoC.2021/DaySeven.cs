4d610b6861ece02e6dac80e3a41d22d66d64b661
Spenser Andreassen <756316@corp.caseys.com>
2021-12-21 12:08:08 -0600

    2021 - day seven part two complete

--- AoC.2021/DaySeven.cs
+++ AoC.2021/DaySeven.cs
@@ -27,8 +27,42 @@ public class DaySeven : IDay
         return fuelAmount;
     }
 
+    private int GetFactorial(int number)
+    {
+        var factorial = 0;
+
+        for (int i = number; i > 0; i--)
+        {
+            factorial += i;
+        }
+
+        return factorial;
+    }
+
     public object PartTwo(string[] input)
     {
-        throw new NotImplementedException();
+        var fuelAmount = int.MaxValue;
+
+        var positions = input.Select(i => Convert.ToInt32(i)).ToList();
+        var maxPosition = positions.Max();
+
+        for (int i = maxPosition; i > 0; i--)
+        {
+            var positionToMoveTo = i;
+            var totalFuel = positions
+                .Select(currentPosition =>
+                {
+                    var distance = Math.Abs(currentPosition - positionToMoveTo);
+                    var factorial = GetFactorial(distance);
+                    return factorial;
+                }).Sum();
+
+            if (totalFuel < fuelAmount)
+            {
+                fuelAmount = totalFuel;
+            }
+        }
+
+        return fuelAmount;
     }
 }
--- AoC.UnitTests/AoC.2021/DaySevenTests.cs
+++ AoC.UnitTests/AoC.2021/DaySevenTests.cs
@@ -42,34 +42,39 @@ public class DaySevenTests
     
         var actual = _underTest.PartOne(input);
     
-        actual.Should().Be(345793);
+        actual.Should().Be(348996);
     }
     
-    // [Fact(DisplayName =
-    //     "day seven part two calculates the number of lanternfish after 256 days")]
-    // public void DaySevenPartTwoTest_One()
-    // {
-    //     var input = new[]
-    //     {
-    //         "3",
-    //         "4",
-    //         "3",
-    //         "1",
-    //         "2"
-    //     };
-    //
-    //     var actual = _underTest.PartTwo(input);
-    //
-    //     actual.Should().Be(26984457539);
-    // }
-    //
-    // [Fact(DisplayName = "day seven part two with puzzle input gets the correct answer")]
-    // public void DaySevenPartTwoTest_Two()
-    // {
-    //     var input = FileReader.ReadAllLines(@"AoC.2021/Data/DaySeven.txt");
-    //
-    //     var actual = _underTest.PartTwo(input);
-    //
-    //     actual.Should().Be(1572643095893L);
-    // }
+    [Fact(DisplayName =
+        "day seven part two calculates the amount of fuel spent aligning the crabs with increasing fuel per step")]
+    public void DaySevenPartTwoTest_One()
+    {
+        var input = new[]
+        {
+            "16",
+            "1",
+            "2",
+            "0",
+            "4",
+            "2",
+            "7",
+            "1",
+            "2",
+            "14"
+        };
+    
+        var actual = _underTest.PartTwo(input);
+    
+        actual.Should().Be(168);
+    }
+    
+    [Fact(DisplayName = "day seven part two with puzzle input gets the correct answer")]
+    public void DaySevenPartTwoTest_Two()
+    {
+        var input = FileReader.ReadAllLines(@"AoC.2021/Data/DaySeven.txt");
+    
+        var actual = _underTest.PartTwo(input);
+    
+        actual.Should().Be(98231647);
+    }
 }
