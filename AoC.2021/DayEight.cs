d4bbdb6e61818d1047921d763904bdca3bd0c2d9
Spenser Andreassen <756316@corp.caseys.com>
2021-12-23 09:02:22 -0600

    2021 - day eight part one complete

---
+++ AoC.2021/DayEight.cs
@@ -0,0 +1,49 @@
+﻿using AoC.Common;
+
+namespace AoC._2021;
+
+public class DayEight : IDay
+{
+    public string Title => "--- Day 8: Seven Segment Search ---";
+
+    private Dictionary<int, string[]> _signalPatternCountToPotentialSegments = new()
+    {
+        { 0, new[] { "a", "b", "c", "e", "f", "g" } },
+        { 1, new[] { "c", "f" } },
+        { 2, new[] { "a", "c", "d", "e", "g" } },
+        { 3, new[] { "a", "c", "d", "f", "g" } },
+        { 4, new[] { "b", "c", "d", "f" } },
+        { 5, new[] { "a", "b", "d", "f", "g" } },
+        { 6, new[] { "a", "b", "d", "e", "f", "g" } },
+        { 7, new[] { "a", "c", "f" } },
+        { 8, new[] { "a", "b", "c", "d", "e", "f", "g" } },
+        { 9, new[] { "a", "b", "c", "d", "f", "g" } }
+    };
+
+    private Dictionary<int, int[]> _signalPatternLengthToDisplayNumbers = new Dictionary<int, int[]>()
+    {
+        { 2, new[] { 1 } },
+        { 3, new[] { 7 } },
+        { 4, new[] { 4 } },
+        { 5, new[] { 2, 3, 5 } },
+        { 6, new[] { 0, 6, 9 } },
+        { 7, new[] { 8 } }
+    };
+
+    public object PartOne(string[] input)
+    {
+        return input.Select(i =>
+            {
+                var inputsAndOutputs = i.Split('|');
+
+                return inputsAndOutputs[1];
+            }).ToList()
+            .SelectMany(o => o.Split(' '))
+            .Count(v => new[] { 2, 3, 4, 7 }.Contains(v.Length));
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2021/DayEightTests.cs
@@ -0,0 +1,81 @@
+﻿using AoC._2021;
+using FluentAssertions;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AoC.UnitTests.AoC._2021;
+
+public class DayEightTests
+{
+    private readonly DayEight _underTest;
+
+    public DayEightTests()
+    {
+        _underTest = new DayEight();
+    }
+
+    [Fact(DisplayName = "day eight part one calculates the number of times 1, 4, 7 or 8 appear in the output")]
+    public void DayEightPartOneTest_One()
+    {
+        var input = new[]
+        {
+            "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+            "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+            "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+            "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+            "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+            "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+            "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+            "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+            "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+            "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(26);
+    }
+
+    [Fact(DisplayName = "day eight part one with puzzle input gets the correct answer")]
+    public void DayEightPartOneTest_Two()
+    {
+        var input = FileReader.ReadAllLines(@"AoC.2021/Data/DayEight.txt");
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(348996);
+    }
+
+    // [Fact(DisplayName =
+    //     "day eight part two calculates the amount of fuel spent aligning the crabs with increasing fuel per step")]
+    // public void DayEightPartTwoTest_One()
+    // {
+    //     var input = new[]
+    //     {
+    //         "16",
+    //         "1",
+    //         "2",
+    //         "0",
+    //         "4",
+    //         "2",
+    //         "7",
+    //         "1",
+    //         "2",
+    //         "14"
+    //     };
+    //
+    //     var actual = _underTest.PartTwo(input);
+    //
+    //     actual.Should().Be(168);
+    // }
+    //
+    // [Fact(DisplayName = "day eight part two with puzzle input gets the correct answer")]
+    // public void DayEightPartTwoTest_Two()
+    // {
+    //     var input = FileReader.ReadAllLines(@"AoC.2021/Data/DayEight.txt");
+    //
+    //     var actual = _underTest.PartTwo(input);
+    //
+    //     actual.Should().Be(98231647);
+    // }
+}
