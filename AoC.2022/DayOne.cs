3c161593f95b3d88cf5520d832e221c740eda2c0
Spenser Andreassen <756316@corp.caseys.com>
2022-12-01 08:17:37 -0600

    2022 day one part one complete

---
+++ AoC.2022/DayOne.cs
@@ -0,0 +1,34 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class DayOne : IDay
+{
+    public string Title => "--- Day 1: Calorie Counting ---";
+
+    public object PartOne(string[] input)
+    {
+        var mostCalories = 0;
+        var currentElfCalories = 0;
+
+        foreach (var elvesCalories in input)
+        {
+            if (elvesCalories == string.Empty)
+            {
+                if (currentElfCalories > mostCalories) mostCalories = currentElfCalories;
+                currentElfCalories = 0;
+            }
+            else
+            {
+                currentElfCalories += int.Parse(elvesCalories);
+            }
+        }
+
+        return mostCalories;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/DayOneTests.cs
@@ -0,0 +1,51 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class DayOneTests
+{
+    private DayOne _underTest;
+
+    public DayOneTests()
+    {
+        _underTest = new DayOne();
+    }
+
+    [Fact(DisplayName = "calculates the most calories held by a single elf with test input")]
+    public void DayOnePartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000"
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(24000);
+    }
+
+    [Fact(DisplayName = "calculates the most calories held by a single elf with actual input")]
+    public void DayOnePartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(@"AoC.2022/Data/DayOne.txt");
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(1);
+    }
+}
