a59fc5ac332ba0d58176c56e8965b9b19527e83f
Spenser Andreassen <756316@corp.caseys.com>
2022-12-02 09:15:43 -0600

    2022 day two part one complete

---
+++ AoC.2022/DayTwo.cs
@@ -0,0 +1,61 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class DayTwo : IDay
+{
+    public string Title => "--- Day 2: Rock Paper Scissors ---";
+
+    private readonly Dictionary<string, RockPaperScissorsScores> _inputActionToGameActionMap = new()
+    {
+        ["A"] = RockPaperScissorsScores.Rock,
+        ["B"] = RockPaperScissorsScores.Paper,
+        ["C"] = RockPaperScissorsScores.Scissors,
+        ["X"] = RockPaperScissorsScores.Rock,
+        ["Y"] = RockPaperScissorsScores.Paper,
+        ["Z"] = RockPaperScissorsScores.Scissors
+    };
+
+    private enum RockPaperScissorsScores
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
+    public object PartOne(string[] input)
+    {
+        var totalScore = 0;
+
+        foreach (var round in input)
+        {
+            var actions = round.Split(' ');
+            var opponentAction = _inputActionToGameActionMap[actions[0]];
+            var myAction = _inputActionToGameActionMap[actions[1]];
+
+            var myRockPaperScissorsScore = GetMyRockPaperScissorsScore(opponentAction, myAction);
+            totalScore += myRockPaperScissorsScore + (int)myAction;
+        }
+
+        return totalScore;
+    }
+
+    private int GetMyRockPaperScissorsScore(RockPaperScissorsScores opponentAction, RockPaperScissorsScores myAction)
+    {
+        // draw
+        if (opponentAction == myAction) return 3;
+
+        // lost
+        if (opponentAction is RockPaperScissorsScores.Rock && myAction is RockPaperScissorsScores.Scissors) return 0;
+        if (opponentAction is RockPaperScissorsScores.Paper && myAction is RockPaperScissorsScores.Rock) return 0;
+        if (opponentAction is RockPaperScissorsScores.Scissors && myAction is RockPaperScissorsScores.Paper) return 0;
+
+        // won
+        return 6;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/DayTwoTests.cs
@@ -0,0 +1,41 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class DayTwoTests
+{
+    private readonly DayTwo _underTest;
+    private const string TestDataFile = @"AoC.2022/Data/DayTwo.txt";
+
+    public DayTwoTests()
+    {
+        _underTest = new DayTwo();
+    }
+
+    [Fact(DisplayName = "calculates total score by following the strategy guide with test input")]
+    public void DayTwoPartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "A Y",
+            "B X",
+            "C Z"
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(15);
+    }
+
+    [Fact(DisplayName = "calculates total score by following the strategy guide with actual input")]
+    public void DayTwoPartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(12458);
+    }
+}
