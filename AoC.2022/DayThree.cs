e17dc99adda387acec632d865f6616ba811db155
Spenser Andreassen <spenserca@gmail.com>
2022-12-03 08:18:00 -0600

    2022 day three part one complete

---
+++ AoC.2022/DayThree.cs
@@ -0,0 +1,57 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class DayThree : IDay
+{
+    public string Title => "--- Day 3: Rucksack Reorganization ---";
+
+    public object PartOne(string[] input)
+    {
+        var totalPriority = 0;
+        
+        foreach (var items in input)
+        {
+            var rucksack = new RuckSack(items);
+            totalPriority += rucksack.GetPriority();
+        }
+
+        return totalPriority;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private class RuckSack
+    {
+        private readonly string _compartmentOne;
+        private readonly string _compartmentTwo;
+
+        public RuckSack(string items)
+        {
+            var splitIndex = items.Length / 2;
+            _compartmentOne = items[..splitIndex];
+            _compartmentTwo = items[splitIndex..];
+        }
+
+        public int GetPriority()
+        {
+            const string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var item = GetSharedItem();
+
+            return priorityList.IndexOf(item, StringComparison.Ordinal) + 1;
+        }
+
+        private string GetSharedItem()
+        {
+            foreach (var item in _compartmentOne)
+            {
+                if (_compartmentTwo.Contains(item)) return item.ToString();
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/DayThreeTests.cs
@@ -0,0 +1,44 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class DayThreeTests
+{
+    private readonly DayThree _underTest;
+    private const string TestDataFile = @"AoC.2022/Data/DayThree.txt";
+
+    public DayThreeTests()
+    {
+        _underTest = new DayThree();
+    }
+
+    [Fact(DisplayName = "calculates the sum of priorities of items included in both compartments with test input")]
+    public void DayThreePartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw",
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(157);
+    }
+    
+    [Fact(DisplayName = "calculates the sum of priorities of items included in both compartments with actual input")]
+    public void DayThreePartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(157);
+    }
+}
