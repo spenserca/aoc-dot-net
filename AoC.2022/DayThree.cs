970a44b68352ea3aefe035f5c5b613b602d37e56
Spenser Andreassen <spenserca@gmail.com>
2022-12-03 09:20:59 -0600

    refactoring away from classes

--- AoC.2022/DayThree.cs
+++ AoC.2022/DayThree.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Common;
+﻿using System.Text;
+using AoC.Common;
 
 namespace AoC._2022;
 
@@ -12,79 +13,73 @@ public class DayThree : IDay
 
         foreach (var items in input)
         {
-            var rucksack = new RuckSack(items);
-            totalPriority += rucksack.GetPriority();
+            var distinctItemsByCompartment = GetRuckSackItemsDistinctByCompartment(items);
+            var sharedItem = GetSharedItem(distinctItemsByCompartment);
+            
+            totalPriority += CalculatePriority(sharedItem);
         }
 
         return totalPriority;
     }
 
+    private static string GetRuckSackItemsDistinctByCompartment(string items)
+    {
+        var splitIndex = items.Length / 2;
+        var distinctItemsInCompartmentOne = string.Join("", items[..splitIndex].Distinct());
+        var distinctItemsInCompartmentTwo = string.Join("", items[splitIndex..].Distinct());
+        return distinctItemsInCompartmentOne + distinctItemsInCompartmentTwo;
+    }
+
+    private static string GetSharedItem(string items)
+    {
+        foreach (var item in items)
+        {
+            if (items.Count(i => i == item) == 2) return item.ToString();
+        }
+        
+        throw new ArgumentException("No item found that matches criteria for shared items!", nameof(items));
+    }
+
     public object PartTwo(string[] input)
     {
         var badgePriority = 0;
-        var currentElfGroupItems = string.Empty;
+        var currentElfGroupItems = new StringBuilder();
 
         for (var i = 0; i < input.Length; i++)
         {
             var distinctItems = string.Join("", input[i].Distinct());
-            currentElfGroupItems += distinctItems;
+            currentElfGroupItems.Append(distinctItems);
 
             if (IsLastElfInGroup(i))
             {
-                const string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                var badgeItem = GetBadgeItem(currentElfGroupItems);
-                badgePriority += priorityList.IndexOf(badgeItem, StringComparison.Ordinal) + 1;
-                
-                currentElfGroupItems = string.Empty;
+                var badgeItem = GetBadgeItem(currentElfGroupItems.ToString());
+                badgePriority += CalculatePriority(badgeItem);
+
+                currentElfGroupItems.Clear();
             }
         }
         
         return badgePriority;
     }
 
-    private string GetBadgeItem(string items)
+    private static int CalculatePriority(string item)
+    {
+        const string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return priorityList.IndexOf(item, StringComparison.Ordinal) + 1;
+    }
+
+    private static string GetBadgeItem(string items)
     {
         foreach (var item in items)
         {
             if (items.Count(i => i == item) == 3) return item.ToString();
         }
 
-        throw new Exception("No item found that matches criteria for badge items!");
+        throw new ArgumentException("No item found that matches criteria for badge items!", nameof(items));
     }
 
     private static bool IsLastElfInGroup(int i)
     {
         return (i + 1) % 3 == 0;
-    }
-
-    private class RuckSack
-    {
-        private readonly string _compartmentOne;
-        private readonly string _compartmentTwo;
-
-        public RuckSack(string items)
-        {
-            var splitIndex = items.Length / 2;
-            _compartmentOne = items[..splitIndex];
-            _compartmentTwo = items[splitIndex..];
-        }
-
-        public int GetPriority()
-        {
-            const string priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var item = GetSharedItem();
-
-            return priorityList.IndexOf(item, StringComparison.Ordinal) + 1;
-        }
-
-        private string GetSharedItem()
-        {
-            foreach (var item in _compartmentOne)
-            {
-                if (_compartmentTwo.Contains(item)) return item.ToString();
-            }
-
-            return string.Empty;
-        }
     }
 }
