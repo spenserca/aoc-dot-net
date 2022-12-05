49569da57bb1b1b95d9fa466e76974059213ae33
Spenser Andreassen <756316@corp.caseys.com>
2022-12-05 09:31:50 -0600

    2022 day four part one complete

---
+++ AoC.2022/Day04.cs
@@ -0,0 +1,40 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class Day04 : IDay
+{
+    public string Title => "--- Day 4: Camp Cleanup ---";
+
+    public object PartOne(string[] input)
+    {
+        var totalRangesContainingOtherRange = 0;
+
+        foreach (var elfPairs in input)
+        {
+            var rawRanges = elfPairs.Split(',');
+            var rawRangeOne = rawRanges[0].Split('-');
+            var rangeOne = new Range(int.Parse(rawRangeOne[0]), int.Parse(rawRangeOne[1]));
+            var rawRangeTwo = rawRanges[1].Split('-');
+            var rangeTwo = new Range(int.Parse(rawRangeTwo[0]), int.Parse(rawRangeTwo[1]));
+
+            if (rangeOne.Contains(rangeTwo) || rangeTwo.Contains(rangeOne))
+                totalRangesContainingOtherRange++;
+        }
+
+        return totalRangesContainingOtherRange;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class RangeExtensions
+{
+    public static bool Contains(this Range a, Range b)
+    {
+        return a.Start.Value <= b.Start.Value && a.End.Value >= b.End.Value;
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/Day04Tests.cs
@@ -0,0 +1,44 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class Day04Tests
+{
+    private readonly Day04 _underTest;
+    private const string TestDataFile = @"AoC.2022/Data/Day04.txt";
+
+    public Day04Tests()
+    {
+        _underTest = new Day04();
+    }
+
+    [Fact(DisplayName = "calculates the number of pairs where one range contains the other with test input")]
+    public void DayFourPartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(2);
+    }
+
+    [Fact(DisplayName = "calculates the number of pairs where one range contains the other with actual input")]
+    public void DayFourPartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(2);
+    }
+}
