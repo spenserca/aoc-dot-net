08d3fbb9173d13ca2dd6d6297c1534a172bd24a9
Spenser Andreassen <756316@corp.caseys.com>
2022-12-06 07:57:16 -0600

    2022 day six part one complete

---
+++ AoC.2022/Day06.cs
@@ -0,0 +1,33 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class Day06 : IDay
+{
+    public string Title => "--- Day 6: Tuning Trouble ---";
+
+    public object PartOne(string[] input)
+    {
+        const int numberOfCharsToCheck = 4;
+        var value = input[0];
+        var charsProcessed = 0;
+        for (var i = 0; i < value.Length - (1 + numberOfCharsToCheck); i++)
+        {
+            var lastFourChars = value.Substring(i, numberOfCharsToCheck);
+            var distinctChars = string.Join("", lastFourChars.Distinct());
+
+            if (distinctChars.Length == 4)
+            {
+                charsProcessed = i + numberOfCharsToCheck;
+                break;
+            }
+        }
+
+        return charsProcessed;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/Day06Tests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class Day06Tests
+{
+    private readonly Day06 _underTest;
+    private const string TestFilePath = @"AoC.2022/Data/Day06.txt";
+
+    public Day06Tests()
+    {
+        _underTest = new Day06();
+    }
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void DaySixPartOne_TestInput(string value, int expected)
+    {
+        var input = new[]
+        {
+            value
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(expected);
+    }
+
+    [Fact(DisplayName = "calculates the number of chars processed until 4 unique chars are found in a row with actual input")]
+    public void DaySixPartOne_ActualInput()
+    {
+        var input = File.ReadAllLines(TestFilePath);
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(1);
+    }
+}
