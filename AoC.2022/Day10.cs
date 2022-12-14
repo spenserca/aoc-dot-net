589da65ec27ac607bca78d48b37b0191377febf6
Spenser Andreassen <756316@corp.caseys.com>
2022-12-14 15:13:51 -0600

    2022 day ten part one complete

---
+++ AoC.2022/Day10.cs
@@ -0,0 +1,63 @@
+﻿using AoC.Common;
+
+namespace AoC._2022;
+
+public class Day10 : IDay
+{
+    public string Title => "--- Day 10: Cathode-Ray Tube ---";
+
+    public object PartOne(string[] input)
+    {
+        var totalSignalStrength = 0;
+        var registerValue = 1;
+        var valueToAdd = 0;
+        var interestingCycles = new[]
+        {
+            20,
+            60,
+            100,
+            140,
+            180,
+            220
+        };
+        var cycles = new List<string>();
+
+        foreach (var instruction in input)
+        {
+            if (instruction == "noop") cycles.Add(instruction);
+            else
+            {
+                cycles.Add(instruction);
+                cycles.Add("X");
+            }
+        }
+
+        for (var i = 1; i <= cycles.Count; i++)
+        {
+            if (interestingCycles.Contains(i))
+            {
+                totalSignalStrength += i * registerValue;
+            }
+
+            var cycleInstruction = cycles[i - 1];
+            if (cycleInstruction == "noop") continue;
+
+            if (cycleInstruction == "X")
+            {
+                registerValue += valueToAdd;
+            }
+            else
+            {
+                var value = int.Parse(cycleInstruction.Split(" ")[1]);
+                valueToAdd = value;
+            }
+        }
+
+        return totalSignalStrength;
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/Day10Tests.cs
@@ -0,0 +1,183 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class Day10Tests
+{
+    private readonly Day10 _underTest;
+
+    public Day10Tests()
+    {
+        _underTest = new Day10();
+    }
+
+    [Fact(DisplayName = "calculates total signal strength with test input")]
+    public void DayTenPartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "addx 15",
+            "addx -11",
+            "addx 6",
+            "addx -3",
+            "addx 5",
+            "addx -1",
+            "addx -8",
+            "addx 13",
+            "addx 4",
+            "noop",
+            "addx -1",
+            "addx 5",
+            "addx -1",
+            "addx 5",
+            "addx -1",
+            "addx 5",
+            "addx -1",
+            "addx 5",
+            "addx -1",
+            "addx -35",
+            "addx 1",
+            "addx 24",
+            "addx -19",
+            "addx 1",
+            "addx 16",
+            "addx -11",
+            "noop",
+            "noop",
+            "addx 21",
+            "addx -15",
+            "noop",
+            "noop",
+            "addx -3",
+            "addx 9",
+            "addx 1",
+            "addx -3",
+            "addx 8",
+            "addx 1",
+            "addx 5",
+            "noop",
+            "noop",
+            "noop",
+            "noop",
+            "noop",
+            "addx -36",
+            "noop",
+            "addx 1",
+            "addx 7",
+            "noop",
+            "noop",
+            "noop",
+            "addx 2",
+            "addx 6",
+            "noop",
+            "noop",
+            "noop",
+            "noop",
+            "noop",
+            "addx 1",
+            "noop",
+            "noop",
+            "addx 7",
+            "addx 1",
+            "noop",
+            "addx -13",
+            "addx 13",
+            "addx 7",
+            "noop",
+            "addx 1",
+            "addx -33",
+            "noop",
+            "noop",
+            "noop",
+            "addx 2",
+            "noop",
+            "noop",
+            "noop",
+            "addx 8",
+            "noop",
+            "addx -1",
+            "addx 2",
+            "addx 1",
+            "noop",
+            "addx 17",
+            "addx -9",
+            "addx 1",
+            "addx 1",
+            "addx -3",
+            "addx 11",
+            "noop",
+            "noop",
+            "addx 1",
+            "noop",
+            "addx 1",
+            "noop",
+            "noop",
+            "addx -13",
+            "addx -19",
+            "addx 1",
+            "addx 3",
+            "addx 26",
+            "addx -30",
+            "addx 12",
+            "addx -1",
+            "addx 3",
+            "addx 1",
+            "noop",
+            "noop",
+            "noop",
+            "addx -9",
+            "addx 18",
+            "addx 1",
+            "addx 2",
+            "noop",
+            "noop",
+            "addx 9",
+            "noop",
+            "noop",
+            "noop",
+            "addx -1",
+            "addx 2",
+            "addx -37",
+            "addx 1",
+            "addx 3",
+            "noop",
+            "addx 15",
+            "addx -21",
+            "addx 22",
+            "addx -6",
+            "addx 1",
+            "noop",
+            "addx 2",
+            "addx 1",
+            "noop",
+            "addx -10",
+            "noop",
+            "noop",
+            "addx 20",
+            "addx 1",
+            "addx 2",
+            "addx 2",
+            "addx -6",
+            "addx -11",
+            "noop",
+            "noop",
+            "noop",
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(13140);
+    }
+
+    [Fact(DisplayName = "calculates total signal strength with actual input")]
+    public void DayTenPartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(@"AoC.2022/Data/Day10.txt");
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(13140);
+    }
+}
