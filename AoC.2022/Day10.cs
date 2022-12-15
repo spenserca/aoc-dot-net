9ae3dc9a8815e498592dbf43b04c08a5ae5a0ec3
Spenser Andreassen <spenserca@gmail.com>
2022-12-14 20:13:33 -0600

    wip

--- AoC.2022/Day10.cs
+++ AoC.2022/Day10.cs
@@ -20,17 +20,7 @@ public class Day10 : IDay
             180,
             220
         };
-        var cycles = new List<string>();
-
-        foreach (var instruction in input)
-        {
-            if (instruction == "noop") cycles.Add(instruction);
-            else
-            {
-                cycles.Add(instruction);
-                cycles.Add("X");
-            }
-        }
+        var cycles = BuildCycles(input);
 
         for (var i = 1; i <= cycles.Count; i++)
         {
@@ -56,8 +46,85 @@ public class Day10 : IDay
         return totalSignalStrength;
     }
 
+    private static List<string> BuildCycles(IEnumerable<string> input)
+    {
+        var cycles = new List<string>();
+
+        foreach (var instruction in input)
+        {
+            if (instruction == "noop") cycles.Add(instruction);
+            else
+            {
+                cycles.Add(instruction);
+                cycles.Add("X");
+            }
+        }
+
+        return cycles;
+    }
+
     public object PartTwo(string[] input)
     {
-        throw new NotImplementedException();
+        var registerValue = 1;
+        var screenLinePosition = 0;
+        var valueToAdd = 0;
+        var screen = BuildScreen();
+        var cycles = BuildCycles(input);
+        var screenLineIndex = 0;
+
+        // if the register value (+-1) is equal to the current cycle
+        // then draw the sprite
+
+        for (var i = 1; i <= cycles.Count; i++)
+        {
+            var isRegisterInSpriteRange = registerValue == screenLinePosition - 1 ||
+                                          registerValue == screenLinePosition ||
+                                          registerValue == screenLinePosition + 1;
+            if (isRegisterInSpriteRange)
+            {
+                var screenLine = screen[screenLineIndex].ToCharArray();
+                screenLine[screenLinePosition] = '#';
+                screen[screenLineIndex] = string.Join("", screenLine);
+            }
+
+            var cycleInstruction = cycles[i - 1];
+            if (cycleInstruction == "noop") continue;
+
+            if (cycleInstruction == "X")
+            {
+                registerValue += valueToAdd;
+            }
+            else
+            {
+                var value = int.Parse(cycleInstruction.Split(" ")[1]);
+                valueToAdd = value;
+            }
+
+            // reset screen line position for next line
+            if (screenLinePosition == 40)
+            {
+                screenLineIndex++;
+                screenLinePosition = 0;
+            }
+
+            screenLinePosition++;
+        }
+
+        return screen.ToArray();
+    }
+
+    private static List<string> BuildScreen()
+    {
+        var screenLines = new List<string>()
+        {
+            "........................................",
+            "........................................",
+            "........................................",
+            "........................................",
+            "........................................",
+            "........................................",
+        };
+
+        return screenLines;
     }
 }
