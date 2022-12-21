c078cfada9efad6a1f5751261e32aaf22278c033
Spenser Andreassen <spenserca@gmail.com>
2022-12-20 20:09:01 -0600

    stubbed out 2022 day 13 part one

---
+++ AoC.2022/Day13.cs
@@ -0,0 +1,18 @@
+using AoC.Common;
+
+namespace AoC._2022;
+
+public class Day13 : IDay
+{
+    public string Title => "--- Day 13: Distress Signal ---";
+
+    public object PartOne(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/Day13Tests.cs
@@ -0,0 +1,50 @@
+using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class Day13Tests
+{
+    private readonly Day13 _underTest;
+
+    public Day13Tests()
+    {
+        _underTest = new Day13();
+    }
+
+    [Fact(DisplayName = "gets a count of all packet pairs that are in the right order with test input")]
+    public void DayThirteenPartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "[1,1,3,1,1]",
+            "[1,1,5,1,1]",
+            "",
+            "[[1],[2,3,4]]",
+            "[[1],4]",
+            "",
+            "[9]",
+            "[[8,7,6]]",
+            "",
+            "[[4,4],4,4]",
+            "[[4,4],4,4,4]",
+            "",
+            "[7,7,7,7]",
+            "[7,7,7]",
+            "",
+            "[]",
+            "[3]",
+            "",
+            "[[[]]]",
+            "[[]]",
+            "",
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+        };
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(13);
+    }
+}
