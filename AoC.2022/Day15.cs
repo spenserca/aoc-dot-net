44dfc8d45fd64934357fb69ccaab11cdc3d573a7
Spenser Andreassen <756316@corp.caseys.com>
2022-12-22 15:37:58 -0600

    wip 2022 day 15

---
+++ AoC.2022/Day15.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+using AoC.Common;
+
+namespace AoC._2022;
+
+public class Day15: IDay
+{
+    public string Title => "--- Day 15: Beacon Exclusion Zone ---";
+
+    public int RowToInspect { get; set; }
+
+    public object PartOne(string[] input)
+    {
+        var sensorBeaconCoordinateRegex = new Regex(@"(x\=[\+|\-]?[0-9]{1,},\sy=[\+|\-]?[0-9]{1,})");
+        var sensors = new List<Sensor>();
+
+        foreach (var sensorDetail in input)
+        {
+            var rawCoordinates = sensorBeaconCoordinateRegex.Matches(sensorDetail);
+            var sensorLocation = ParseCoordinate(rawCoordinates[0]);
+            var beaconLocation = ParseCoordinate(rawCoordinates[1]);
+
+            sensors.Add(new Sensor(sensorLocation, beaconLocation, RowToInspect));
+        }
+
+        return sensors.Select(s => s.NumberOfLocationsWhereABeaconIsNotPresent).Sum();
+    }
+
+    private static Coordinate ParseCoordinate(Capture rawCoordinateMatch)
+    {
+        var rawCoordinate = rawCoordinateMatch.Value;
+
+        var rawCoordinatePieces = rawCoordinate.Split("=");
+        var x = int.Parse(rawCoordinatePieces[1].Split(',')[0]);
+        var y = int.Parse(rawCoordinatePieces[2]);
+
+        return new Coordinate(x, y);
+    }
+
+    public object PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private class Sensor
+    {
+        private readonly Coordinate _location;
+        private readonly Coordinate _closestBeacon;
+        private readonly List<Coordinate> _coordinatesInRowToInspect;
+
+        private int manhattanDistanceToBeacon =>
+            Math.Abs(_location.X - _closestBeacon.X) + Math.Abs(_location.Y - _closestBeacon.Y);
+
+        public Sensor(Coordinate location, Coordinate closestBeacon, int rowToInspect)
+        {
+            _location = location;
+            _closestBeacon = closestBeacon;
+            _coordinatesInRowToInspect = SetPointsThatExistInRowToInspect(rowToInspect);
+        }
+
+        private List<Coordinate> SetPointsThatExistInRowToInspect(int rowToInspect)
+        {
+            // use manhattan distance to determine the radius of the sensor
+
+            // if any points of the radius are in the row to inspect, add them to the list
+            // otherwise skip them
+
+            return new List<Coordinate>();
+        }
+
+        public int NumberOfLocationsWhereABeaconIsNotPresent => _coordinatesInRowToInspect.Count;
+    }
+}
---
+++ AoC.UnitTests/AoC.2022/Day15Tests.cs
@@ -0,0 +1,43 @@
+﻿using AoC._2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2022;
+
+public class Day15Tests
+{
+    private readonly Day15 _underTest;
+    private readonly string[] _actualInput = FileReader.ReadAllLines(@"AoC.2022/Data/Day15.txt");
+
+    public Day15Tests()
+    {
+        _underTest = new Day15();
+    }
+
+    [Fact(DisplayName = "")]
+    public void DayFifteenPartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+            "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+            "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+            "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+            "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+            "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+            "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+            "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+            "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+            "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+            "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+            "Sensor at x=20, y=1: closest beacon is at x=15, y=3"
+        };
+
+        _underTest.RowToInspect = 10;
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(26);
+    }
+}
