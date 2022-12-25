24ba84d219d81f735c139bf8d8d322ee360a9ad1
Spenser Andreassen <spenserca@gmail.com>
2022-12-25 17:32:15 -0600

    refactoring to common extension methods

--- AoC.Common/StringArrayExtensions.cs
+++ AoC.Common/StringArrayExtensions.cs
@@ -15,10 +15,24 @@ public static class StringArrayExtensions
         throw new PointNotFoundException($"Couldn't find a point with the value {value}");
     }
 
-    public static List<Coordinate> GetSurroundingCoordinates(this Coordinate coordinate, string[] grid)
+    public static List<Coordinate> GetSurroundingCoordinates(this string[] input, Coordinate coordinate)
     {
-        return new List<Coordinate>();
+        var surroundingCoordinates = new List<Coordinate>
+        {
+            new(coordinate.X + 1, coordinate.Y),
+            new(coordinate.X, coordinate.Y + 1),
+            new(coordinate.X - 1, coordinate.Y),
+            new(coordinate.X, coordinate.Y - 1)
+        };
+
+        return surroundingCoordinates.Where(c => IsInYRange(input, c) && IsInXRange(input, c)).ToList();
     }
+
+    private static bool IsInXRange(IReadOnlyList<string> input, Coordinate coordinate) =>
+        coordinate.X >= 0 && coordinate.X < input[0].Length;
+
+    private static bool IsInYRange(string[] input, Coordinate coordinate) =>
+        coordinate.Y >= 0 && coordinate.Y < input.Length;
 }
 
 internal class PointNotFoundException : Exception
--- AoC.UnitTests/Common/StringArrayExtensionsTests.cs
+++ AoC.UnitTests/Common/StringArrayExtensionsTests.cs
@@ -1,5 +1,6 @@
 using AoC.Common;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Xunit;
 
 namespace AoC.UnitTests.Common;
@@ -17,6 +18,8 @@ public class StringArrayExtensionsTests
 
     private static readonly string[] ActualInput = FileReader.ReadAllLines(@"AoC.2022/Data/Day12.txt");
 
+    #region GetPositionOfValue
+
     [Theory(DisplayName = "gets the coordinate position of the value in the actual input")]
     [InlineData('S', 0, 20, 0, false)]
     [InlineData('E', 119, 20, 25, false)]
@@ -33,4 +36,151 @@ public class StringArrayExtensionsTests
         actual.Y.Should().Be(y);
         actual.Z.Should().Be(z);
     }
+
+    #endregion
+
+    #region GetSurroundingCoordinates
+
+    [Fact(DisplayName = "when point is top left corner, has 2 surrounding values")]
+    public void GetSurroundingCoordinates_TopLeftCorner_ReturnsTwoValidCoordinates()
+    {
+        var coordinate = new Coordinate(0, 0);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(2);
+            actual.Should().Contain(new Coordinate(0, 1));
+            actual.Should().Contain(new Coordinate(1, 0));
+        }
+    }
+
+    [Fact(DisplayName = "when point is top right corner, has 2 surrounding values")]
+    public void GetSurroundingCoordinates_TopRightCorner_ReturnsTwoValidCoordinates()
+    {
+        var coordinate = new Coordinate(7, 0);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(2);
+            actual.Should().Contain(new Coordinate(7, 1));
+            actual.Should().Contain(new Coordinate(6, 0));
+        }
+    }
+
+    [Fact(DisplayName = "when point is bottom left corner, has 2 surrounding values")]
+    public void GetSurroundingCoordinates_BottomLeftCorner_ReturnsTwoValidCoordinates()
+    {
+        var coordinate = new Coordinate(0, 4);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(2);
+            actual.Should().Contain(new Coordinate(0, 3));
+            actual.Should().Contain(new Coordinate(1, 4));
+        }
+    }
+
+    [Fact(DisplayName = "when point is bottom right corner, has 2 surrounding values")]
+    public void GetSurroundingCoordinates_BottomRightCorner_ReturnsTwoValidCoordinates()
+    {
+        var coordinate = new Coordinate(7, 4);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(2);
+            actual.Should().Contain(new Coordinate(7, 3));
+            actual.Should().Contain(new Coordinate(6, 4));
+        }
+    }
+    
+    [Fact(DisplayName = "when point is in the top row (not on a corner), has 3 surrounding values")]
+    public void GetSurroundingCoordinates_TopRow_ReturnsThreeCoordinates()
+    {
+        var coordinate = new Coordinate(4, 0);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(3);
+            actual.Should().Contain(new Coordinate(5, 0));
+            actual.Should().Contain(new Coordinate(3, 0));
+            actual.Should().Contain(new Coordinate(4, 1));
+        }
+    }
+    
+    [Fact(DisplayName = "when point is in the bottom row (not on a corner), has 3 surrounding values")]
+    public void GetSurroundingCoordinates_BottomRow_ReturnsThreeCoordinates()
+    {
+        var coordinate = new Coordinate(4, 4);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(3);
+            actual.Should().Contain(new Coordinate(5, 4));
+            actual.Should().Contain(new Coordinate(3, 4));
+            actual.Should().Contain(new Coordinate(4, 3));
+        }
+    }
+    
+    [Fact(DisplayName = "when point is on the left edge (not on a corner), has 3 surrounding values")]
+    public void GetSurroundingCoordinates_LeftEdge_ReturnsThreeCoordinates()
+    {
+        var coordinate = new Coordinate(0, 2);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(3);
+            actual.Should().Contain(new Coordinate(0, 1));
+            actual.Should().Contain(new Coordinate(0, 3));
+            actual.Should().Contain(new Coordinate(1, 2));
+        }
+    }
+    
+    [Fact(DisplayName = "when point is on the right edge (not on a corner), has 3 surrounding values")]
+    public void GetSurroundingCoordinates_RightEdge_ReturnsThreeCoordinates()
+    {
+        var coordinate = new Coordinate(7, 2);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(3);
+            actual.Should().Contain(new Coordinate(7, 1));
+            actual.Should().Contain(new Coordinate(7, 3));
+            actual.Should().Contain(new Coordinate(6, 2));
+        }
+    }
+    
+    [Fact(DisplayName = "when point is not on the edge, has 4 surrounding values")]
+    public void GetSurroundingCoordinates_NotOnEdge_ReturnsThreeCoordinates()
+    {
+        var coordinate = new Coordinate(4, 3);
+
+        var actual = TestInput.GetSurroundingCoordinates(coordinate);
+
+        using (new AssertionScope())
+        {
+            actual.Count.Should().Be(4);
+            actual.Should().Contain(new Coordinate(3, 3));
+            actual.Should().Contain(new Coordinate(5, 3));
+            actual.Should().Contain(new Coordinate(4, 4));
+            actual.Should().Contain(new Coordinate(4, 2));
+        }
+    }
+
+    #endregion
 }
