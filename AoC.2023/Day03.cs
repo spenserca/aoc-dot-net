4f30a7d51d8f0291d97e15f5e882d183df6022ec
Spenser Andreassen <spenserca@gmail.com>
2023-12-04 17:06:56 -0600

    wip: stubbing out a coordinate grid class

---
+++ AoC.2023/Day03.cs
@@ -0,0 +1,66 @@
+﻿using AoC.Common;
+
+namespace AoC._2023;
+
+public class Day03: IDayPartOne
+{
+    public string Title => "--- Day 3: Gear Ratios ---";
+
+    private static readonly string[] Symbols = { "*", "#", "+", "$" };
+
+    public object PartOne(string[] input)
+    {
+        var enginePart = string.Empty;
+        var isSymbolAdjacent = false;
+        var sum = 0;
+        
+        for (int y = 0; y < input.Length; y++)
+        {
+            var row = input[y];
+            
+            for (int x = 0; x < row.Length; x++)
+            {
+                var value = row[x];
+
+                if (int.TryParse(value.ToString(), out _))
+                {
+                    enginePart += value;
+                    
+                    // check if adjacent
+                    if (HasAdjacentSymbols(input, x, y)) isSymbolAdjacent = true;
+                }
+                else
+                {
+                    if (isSymbolAdjacent)
+                    {
+                        sum += int.Parse(enginePart);
+                        isSymbolAdjacent = false;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+    
+    private static bool HasAdjacentSymbols(string[] input, int x, int y)
+    {
+        var surroundingCoords = GetSurroundingCoords(x, y, input.Length);
+
+        return false;
+    }
+
+    private static IEnumerable<(int x, int y)> GetSurroundingCoords(int x, int y, int maxY)
+    {
+        if (x == 0)
+        {
+            if (y == 0)
+            {
+                return new (int x, int y)[] { (1, y), (1, 1), (x, 1) };
+            } else if (y == maxY)
+            {
+                return new (int x, int y)[] { (1, y), (1, y - 1), (1, y - 1) };
+            }
+        }
+    }
+}
---
+++ AoC.Common/CoordinateGrid.cs
@@ -0,0 +1,52 @@
+﻿namespace AoC.Common;
+
+public class CoordinateGrid
+{
+    private readonly string[] _input;
+
+    private readonly List<Coordinate> _coordinates = new List<Coordinate>();
+
+    public CoordinateGrid(IReadOnlyList<string> input)
+    {
+        for (var y = 0; y < input.Count; y++)
+        {
+            var row = input[y];
+
+            for (var x = 0; x < row.Length; x++)
+            {
+                _coordinates.Add(new Coordinate(x, y, row[x].ToString()));
+            }
+        }
+    }
+
+    public IEnumerable<Coordinate> GetSurroundingValues(Coordinate coordinate)
+    {
+        return _coordinates.Where(c =>
+        {
+            var isToTheLeft = c.X == coordinate.X - 1 && c.Y == coordinate.Y;
+            var isToTheRight = c.X == coordinate.X + 1 && c.Y == coordinate.Y;
+            var isAbove = c.X == coordinate.X && c.Y == coordinate.Y - 1;
+            var isBelow = c.X == coordinate.X && c.Y == coordinate.Y + 1;
+            var isToTheUpperLeft = false;
+            var isToTheUpperRight = false;
+            var isToTheLowerLeft = false;
+            var isToTheLowerRight = false;
+
+            return isToTheLeft || isToTheRight || isAbove || isBelow;
+        });
+    }
+}
+
+public class Coordinate
+{
+    public int X { get; }
+    public int Y { get; }
+    public string Value { get; }
+
+    public Coordinate(int x, int y, string value)
+    {
+        X = x;
+        Y = y;
+        Value = value;
+    }
+}
