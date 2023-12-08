ee9d3d778b2ac25d3657884ecad995b0b0fd0ea7
Spenser Andreassen <756316@corp.caseys.com>
2023-12-08 07:42:34 -0600

    2023 day 4 part 2 complete

--- AoC.2023/Day04.cs
+++ AoC.2023/Day04.cs
@@ -2,7 +2,7 @@
 
 namespace AoC._2023;
 
-public class Day04 : IDayPartOne
+public class Day04 : IDayPartOne, IDayPartTwo
 {
     public string Title => "--- Day 4: Scratchcards ---";
 
@@ -30,5 +30,56 @@ public class Day04 : IDayPartOne
                 return (int)Math.Pow(2, overlap - 1);
             })
             .Sum();
+    }
+
+    public object PartTwo(string[] input)
+    {
+        return input.Select((i, index) => i.Substring(i.IndexOf(':') + 2))
+            .Select((g, index) => new ScratchCard(input[index], index)).ToList()
+            .Sum(c => c.GetCountOfAllCardsWon(input));
+    }
+
+    private class ScratchCard
+    {
+        private int Index { get; }
+
+        private IEnumerable<int> WinningNumbers { get; }
+
+        private IEnumerable<int> CardNumbers { get; }
+
+        private int CountOfMatchedNumbers { get; }
+
+        public ScratchCard(string card, int index)
+        {
+            Index = index;
+
+            var cardSplit = card.Split('|')
+                .Select(c => c.Trim())
+                .ToList();
+
+            WinningNumbers = cardSplit[0].Split(' ')
+                .Where(v => int.TryParse(v, out _))
+                .Select(int.Parse);
+
+            CardNumbers = cardSplit[1].Split(' ')
+                .Where(v => int.TryParse(v, out _))
+                .Select(int.Parse);
+
+            CountOfMatchedNumbers = CardNumbers.Count(c => WinningNumbers.Contains(c));
+        }
+
+        public int GetCountOfAllCardsWon(string[] input)
+        {
+            const int countForCurrentCard = 1;
+            var countOfCardsWonFromBonusCards = 0;
+            for (var i = 0; i < CountOfMatchedNumbers; i++)
+            {
+                var indexOfNextWonCard = Index + 1 + i;
+                var cardsWonFromBonusCard = new ScratchCard(input[indexOfNextWonCard], indexOfNextWonCard);
+                countOfCardsWonFromBonusCards += cardsWonFromBonusCard.GetCountOfAllCardsWon(input);
+            }
+
+            return countForCurrentCard + countOfCardsWonFromBonusCards;
+        }
     }
 }
--- AoC.UnitTests/AoC.2023/Day04Tests.cs
+++ AoC.UnitTests/AoC.2023/Day04Tests.cs
@@ -7,22 +7,21 @@ namespace AoC.UnitTests.AoC._2023;
 public class Day04Tests
 {
     private readonly Day04 _underTest = new();
-    private const string TestDataFile = @"AoC.2023/Data/Day04.txt";
+    private const string TestDataFile = "AoC.2023/Data/Day04.txt";
+    private readonly string[] _testInput = new[]
+    {
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+    };
 
     [Fact(DisplayName = "gets the total points of all the scratch cards with test input")]
     public void DayFourPartOne_TestInput()
     {
-        var input = new[]
-        {
-            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-        };
-
-        var actual = _underTest.PartOne(input);
+        var actual = _underTest.PartOne(_testInput);
 
         actual.Should().Be(13);
     }
@@ -35,5 +34,23 @@ public class Day04Tests
         var actual = _underTest.PartOne(input);
     
         actual.Should().Be(18519);
-    } 
+    }
+
+    [Fact(DisplayName = "gets the total number of scratch cards with test input")]
+    public void DayOnePartTwo_TestInput()
+    {
+        var actual = _underTest.PartTwo(_testInput);
+
+        actual.Should().Be(30);
+    }
+
+    [Fact(DisplayName = "gets the total number of scratch cards with actual input", Skip = "takes 2 minutes to run")]
+    public void DayOnePartTwo_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartTwo(input);
+
+        actual.Should().Be(11787590);
+    }
 }
