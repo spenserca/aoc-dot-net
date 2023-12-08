9794792e5c01f6212b590922a40367f72b4f567d
Spenser Andreassen <756316@corp.caseys.com>
2023-12-08 10:54:01 -0600

    wip

---
+++ AoC.2023/Day05.cs
@@ -0,0 +1,26 @@
+﻿using AoC.Common;
+
+namespace AoC._2023;
+
+public class Day05: IDayPartOne
+{
+    public string Title => "--- Day 5: If You Give A Seed A Fertilizer ---";
+
+    public object PartOne(string[] input)
+    {
+        var seedsToPlant = input[0].Split(' ')
+            .Where(v => int.TryParse(v, out var value))
+            .Select(int.Parse)
+            .ToList();
+        var maps = new Dictionary<string, SourceDestinationRange>()
+
+        return 0;
+    }
+
+    private class SourceDestinationRange
+    {
+        public (int, int) SourceRange { get; set; }
+
+        public (int, int) DestinationRange { get; set; }
+    }
+}
