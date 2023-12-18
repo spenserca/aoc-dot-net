10d3947cc745af1bc93c6cdc083e513230f00892
Spenser Andreassen <spenserca@gmail.com>
2023-12-18 15:29:45 -0600

    2023 day 6 complete

--- AoC.2023/Day06.cs
+++ AoC.2023/Day06.cs
@@ -2,7 +2,7 @@
 
 namespace AoC._2023;
 
-public class Day06 : IDayPartOne
+public class Day06 : IDayPartOne, IDayPartTwo
 {
     public string Title => "--- Day 6: Wait For It ---";
 
@@ -18,15 +18,28 @@ public class Day06 : IDayPartOne
             .ToArray();
         var races = raceDurations.Select((r, index) => new Race(r, recordDistances[index]));
 
-
         return races.Select(r => r.NumberOfWaysToWin())
             .Aggregate(1, (a, b) => a * b);
     }
 
-    private class Race
+    public object PartTwo(string[] input)
     {
-        private readonly int _duration;
-        private readonly int _recordDistance;
+        var raceDuration = string.Join("", input[0].Split(' ')
+            .Where(v => int.TryParse(v, out _))
+            .Select(int.Parse)
+            .ToArray());
+        var recordDistance = string.Join("", input[1].Split(' ')
+            .Where(v => int.TryParse(v, out _))
+            .Select(int.Parse)
+            .ToArray());
+
+        return new Race(long.Parse(raceDuration), long.Parse(recordDistance)).NumberOfWaysToWin();
+    }
+
+    private sealed class Race
+    {
+        private readonly long _duration;
+        private readonly long _recordDistance;
 
         public Race(int duration, int recordDistance)
         {
@@ -34,10 +47,16 @@ public class Day06 : IDayPartOne
             _recordDistance = recordDistance;
         }
 
+        public Race(long duration, long recordDistance)
+        {
+            _duration = duration;
+            _recordDistance = recordDistance;
+        }
+
         public int NumberOfWaysToWin()
         {
-            var numberOfWaysToWin = new List<int>();
-            for (int i = 0; i <= _duration; i++)
+            var numberOfWaysToWin = new List<long>();
+            for (var i = 0; i <= _duration; i++)
             {
                 var distanceTravelled = GetDistanceTravelled(i);
                 if (distanceTravelled > _recordDistance) numberOfWaysToWin.Add(distanceTravelled);
@@ -46,7 +65,7 @@ public class Day06 : IDayPartOne
             return numberOfWaysToWin.Count;
         }
 
-        private int GetDistanceTravelled(int speed)
+        private long GetDistanceTravelled(int speed)
         {
             var numberOfMillisTravelling = _duration - speed;
 
--- AoC.UnitTests/AoC.2023/Day06Tests.cs
+++ AoC.UnitTests/AoC.2023/Day06Tests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace AoC.UnitTests.AoC._2023;
 
-public class Day06Tests : IDayPartOneTest
+public class Day06Tests : IDayPartOneTest, IDayPartTwoTest
 {
     private readonly Day06 _underTest = new();
     private const string TestDataFile = "AoC.2023/Data/Day06.txt";
@@ -30,6 +30,24 @@ public class Day06Tests : IDayPartOneTest
         
         var actual = _underTest.PartOne(input);
 
-        actual.Should().Be(288);
+        actual.Should().Be(3316275);
+    }
+
+    [Fact(DisplayName = "gets the number of ways to win for the race with test input")]
+    public void PartTwo_TestInput()
+    {
+        var actual = _underTest.PartTwo(_testInput);
+
+        actual.Should().Be(71503);
+    }
+
+    [Fact(DisplayName = "gets the number of ways to win for the race with actual input")]
+    public void PartTwo_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+        
+        var actual = _underTest.PartTwo(input);
+
+        actual.Should().Be(27102791);
     }
 }
