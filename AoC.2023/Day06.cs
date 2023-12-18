fc366e14e6ddb23f38e426fe5467194d5740f511
Spenser Andreassen <spenserca@gmail.com>
2023-12-18 14:53:07 -0600

    2023 day 6 part 1 complete

---
+++ AoC.2023/Day06.cs
@@ -0,0 +1,56 @@
+﻿using AoC.Common;
+
+namespace AoC._2023;
+
+public class Day06 : IDayPartOne
+{
+    public string Title => "--- Day 6: Wait For It ---";
+
+    public object PartOne(string[] input)
+    {
+        var raceDurations = input[0].Split(' ')
+            .Where(v => int.TryParse(v, out _))
+            .Select(int.Parse)
+            .ToArray();
+        var recordDistances = input[1].Split(' ')
+            .Where(v => int.TryParse(v, out _))
+            .Select(int.Parse)
+            .ToArray();
+        var races = raceDurations.Select((r, index) => new Race(r, recordDistances[index]));
+
+
+        return races.Select(r => r.NumberOfWaysToWin())
+            .Aggregate(1, (a, b) => a * b);
+    }
+
+    private class Race
+    {
+        private readonly int _duration;
+        private readonly int _recordDistance;
+
+        public Race(int duration, int recordDistance)
+        {
+            _duration = duration;
+            _recordDistance = recordDistance;
+        }
+
+        public int NumberOfWaysToWin()
+        {
+            var numberOfWaysToWin = new List<int>();
+            for (int i = 0; i <= _duration; i++)
+            {
+                var distanceTravelled = GetDistanceTravelled(i);
+                if (distanceTravelled > _recordDistance) numberOfWaysToWin.Add(distanceTravelled);
+            }
+
+            return numberOfWaysToWin.Count;
+        }
+
+        private int GetDistanceTravelled(int speed)
+        {
+            var numberOfMillisTravelling = _duration - speed;
+
+            return speed * numberOfMillisTravelling;
+        }
+    }
+}
---
+++ AoC.UnitTests/AoC.2023/Day06Tests.cs
@@ -0,0 +1,35 @@
+﻿using AoC._2023;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2023;
+
+public class Day06Tests : IDayPartOneTest
+{
+    private readonly Day06 _underTest = new();
+    private const string TestDataFile = "AoC.2023/Data/Day06.txt";
+
+    private readonly string[] _testInput =
+    {
+        "Time:      7  15   30",
+        "Distance:  9  40  200"
+    };
+
+    [Fact(DisplayName = "gets the multiplied value of all ways to win for all races with test input")]
+    public void PartOne_TestInput()
+    {
+        var actual = _underTest.PartOne(_testInput);
+
+        actual.Should().Be(288);
+    }
+
+    [Fact(DisplayName = "gets the multiplied value of all ways to win for all races with actual input")]
+    public void PartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+        
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(288);
+    }
+}
