9459d22ed8e15a21dacf868fc6eba17a847fcf52
Spenser Andreassen <756316@corp.caseys.com>
2023-12-19 15:07:54 -0600

    2023 day 9 part 1 wip

---
+++ AoC.2023/Day09.cs
@@ -0,0 +1,26 @@
+﻿using AoC.Common;
+
+namespace AoC._2023;
+
+public class Day09: IDayPartOne
+{
+    public string Title => "--- Day 9: Mirage Maintenance ---";
+
+    public object PartOne(string[] input)
+    {
+        foreach (var i in input)
+        {
+            var sequence = i.Split(' ').ToList();
+
+            while (!sequence.TrueForAll(v => v.Equals("0")))
+            {
+                sequence = GetNextSequence(sequence);
+            }
+        }
+    }
+
+    private List<string> GetNextSequence(List<string> sequence)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC.UnitTests/AoC.2023/Day09Tests.cs
@@ -0,0 +1,31 @@
+﻿using AoC._2023;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2023;
+
+public class Day09Tests : IDayPartOneTest
+{
+    private readonly Day09 _underTest = new();
+    private const string TestDataFile = "AoC.2023/Data/Day09.txt";
+
+    private readonly string[] _testInput =
+    {
+        "0 3 6 9 12 15",
+        "1 3 6 10 15 21",
+        "10 13 16 21 30 45",
+    };
+
+    [Fact(DisplayName = "calculates the sum of all extrapolated values with test input")]
+    public void PartOne_TestInput()
+    {
+        var actual = _underTest.PartOne(_testInput);
+
+        actual.Should().Be(114);
+    }
+
+    public void PartOne_ActualInput()
+    {
+        throw new System.NotImplementedException();
+    }
+}
