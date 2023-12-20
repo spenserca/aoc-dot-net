17f822dad3c58d2dec64e36cd2f6d4215b8c1ed1
Spenser Andreassen <spenserca@users.noreply.github.com>
2023-12-20 12:52:03 -0600

    2023 day 9 complete (#12)
    * 2023 day 9 part 1 wip
    
    * wip
    
    * wip
    
    * wip
    
    * 2023 day 9 part 1 complete
    
    * 2023 day 9 complete
    
    * formatting
    
    ---------
    
    Co-authored-by: Spenser Andreassen <756316@corp.caseys.com>

---
+++ AoC.2023/Day09.cs
@@ -0,0 +1,84 @@
+﻿using AoC.Common;
+
+namespace AoC._2023;
+
+public class Day09 : IDayPartOne, IDayPartTwo
+{
+    public string Title => "--- Day 9: Mirage Maintenance ---";
+
+    public object PartOne(string[] input)
+    {
+        return input.Select(GetNextValueInHistory).Sum();
+    }
+
+    public object PartTwo(string[] input)
+    {
+        return input.Select(GetEarlierValueInHistory).ToList().Sum();
+    }
+
+    private static int GetEarlierValueInHistory(string history)
+    {
+        var sequences = new List<List<int>>();
+        var sequence = history.Split(' ').Select(int.Parse).ToList();
+
+        sequences.Add(sequence);
+
+        while (!sequence.TrueForAll(v => v == 0))
+        {
+            sequence = GetNextSequence(sequence);
+            sequences.Add(sequence);
+        }
+
+        for (var i = sequences.Count - 1; i > 0; i--)
+        {
+            var sequenceBelowFirstValue = sequences[i].First();
+            var sequenceAboveFirstValue = sequences[i - 1].First();
+
+            var aboveSequenceEarlierValue = sequenceAboveFirstValue - sequenceBelowFirstValue;
+            sequences[i - 1].Insert(0, aboveSequenceEarlierValue);
+        }
+
+        return sequences[0].First();
+    }
+
+    private static int GetNextValueInHistory(string history)
+    {
+        var sequences = new List<List<int>>();
+        var sequence = history.Split(' ').Select(int.Parse).ToList();
+
+        sequences.Add(sequence);
+
+        while (!sequence.TrueForAll(v => v == 0))
+        {
+            sequence = GetNextSequence(sequence);
+            sequences.Add(sequence);
+        }
+
+        for (var i = sequences.Count - 1; i > 0; i--)
+        {
+            var sequenceBelowLastValue = sequences[i].Last();
+            var sequenceAboveLastValue = sequences[i - 1].Last();
+
+            var aboveSequenceNextValue = sequenceBelowLastValue + sequenceAboveLastValue;
+            sequences[i - 1].Add(aboveSequenceNextValue);
+        }
+
+        return sequences[0].Last();
+    }
+
+    private static List<int> GetNextSequence(IReadOnlyList<int> sequence)
+    {
+        var nextSequence = new List<int>();
+        for (var i = 0; i < sequence.Count - 1; i++)
+        {
+            var a = sequence[i];
+            var b = sequence[i + 1];
+            nextSequence.Add(b - a);
+        }
+
+        if (nextSequence.TrueForAll(v => v == 0))
+            nextSequence.Add(0);
+
+        return nextSequence;
+    }
+}
---
+++ AoC.UnitTests/AoC.2023/Day09Tests.cs
@@ -0,0 +1,56 @@
+﻿using AoC._2023;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC.UnitTests.AoC._2023;
+
+public class Day09Tests : IDayPartOneTest, IDayPartTwoTest
+{
+    private readonly Day09 _underTest = new();
+    private const string TestDataFile = "AoC.2023/Data/Day09.txt";
+
+    private readonly string[] _testInput =
+    {
+        "0 3 6 9 12 15",
+        "1 3 6 10 15 21",
+        "10 13 16 21 30 45",
+    };
+
+    [Fact(DisplayName = "calculates the sum of all extrapolated values with test input")]
+    public void PartOne_TestInput()
+    {
+        var actual = _underTest.PartOne(_testInput);
+
+        actual.Should().Be(114);
+    }
+
+    [Fact(DisplayName = "calculates the sum of all extrapolated values with actual input")]
+    public void PartOne_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartOne(input);
+
+        actual.Should().Be(1972648895);
+    }
+
+    [Fact(DisplayName = "calculates the sum of all backwards extrapolated values with test input")]
+    public void PartTwo_TestInput()
+    {
+        var actual = _underTest.PartTwo(_testInput);
+
+        actual.Should().Be(2);
+    }
+
+    [Fact(
+        DisplayName = "calculates the sum of all backwards extrapolated values with actual input"
+    )]
+    public void PartTwo_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _underTest.PartTwo(input);
+
+        actual.Should().Be(919);
+    }
+}
