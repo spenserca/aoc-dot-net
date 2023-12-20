26a5ff2e321c046e88e829543ec05ac8108a2312
Spenser Andreassen <spenserca@gmail.com>
2023-12-19 21:15:34 -0600

    wip

--- AoC.2023/Day09.cs
+++ AoC.2023/Day09.cs
@@ -2,12 +2,14 @@
 
 namespace AoC._2023;
 
-public class Day09: IDayPartOne
+public class Day09 : IDayPartOne
 {
     public string Title => "--- Day 9: Mirage Maintenance ---";
 
     public object PartOne(string[] input)
     {
+        return input.Select(GetNextValueInHistory).Sum();
+
         foreach (var i in input)
         {
             var sequence = i.Split(' ').ToList();
@@ -17,6 +19,17 @@ public class Day09: IDayPartOne
                 sequence = GetNextSequence(sequence);
             }
         }
+
+        return 0;
+    }
+
+    private int GetNextValueInHistory(string history)
+    {
+        var sequence = history.Split(' ')
+            .Select(int.Parse)
+            .Aggregate(0, (x, y) => x - y);
+
+        return 0;
     }
 
     private List<string> GetNextSequence(List<string> sequence)
