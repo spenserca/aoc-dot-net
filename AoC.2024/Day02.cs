a51119ffd1071fd2fb8ea8de8bdb20a823be6e40
Spenser Andreassen <spenserca@gmail.com>
2024-12-02 13:36:41 -0600

    2024 day 2 part 2 complete

--- AoC.2024/Day02.cs
+++ AoC.2024/Day02.cs
@@ -2,7 +2,7 @@ using AoC.Common;
 
 namespace AoC._2024;
 
-public class Day02 : IDayPartOne
+public class Day02 : IDayPartOne, IDayPartTwo
 {
     public string Title => "--- Day 2: Red-Nosed Reports ---";
 
@@ -10,26 +10,59 @@ public class Day02 : IDayPartOne
     {
         return input.Select(i => new Report(i)).Count(r => r.IsSafe());
     }
+
+    public object PartTwo(string[] input)
+    {
+        return input.Select(i => new Report(i)).Count(r => r.IsSafe() || r.IsSafeAfterDampening());
+    }
 }
 
 public class Report(string value)
 {
-    private readonly List<int> _levels = value.Split(" ").Select(int.Parse).ToList();
+    private readonly List<Level> _levels = value
+        .Split(" ")
+        .Select(int.Parse)
+        .Select(v => new Level(v))
+        .ToList();
 
     public bool IsSafe()
     {
-        var isAscendingOrder = _levels.SequenceEqual(_levels.OrderBy(n => n));
-        var isDescendingOrder = _levels.SequenceEqual(_levels.OrderByDescending(n => n));
+        return IsSafe(_levels);
+    }
+
+    private static bool IsSafe(List<Level> levels)
+    {
+        var isAscendingOrder = levels.SequenceEqual(levels.OrderBy(n => n.Value));
+        var isDescendingOrder = levels.SequenceEqual(levels.OrderByDescending(n => n.Value));
         var isDiffNoLargerThanThree = true;
-        for (int i = 0; i < _levels.Count - 1; i++)
+        for (var i = 0; i < levels.Count - 1; i++)
         {
-            var first = _levels[i];
-            var second = _levels[i + 1];
-            var diff = Math.Abs(first - second);
-            if (diff == 0 || diff > 3)
+            var first = levels[i];
+            var second = levels[i + 1];
+            var diff = Math.Abs(first.Value - second.Value);
+            if (diff is 0 or > 3)
+            {
                 isDiffNoLargerThanThree = false;
+            }
         }
 
         return (isAscendingOrder || isDescendingOrder) && isDiffNoLargerThanThree;
     }
+
+    public bool IsSafeAfterDampening()
+    {
+        var isSafeAfterDampening = false;
+        for (var i = 0; i < _levels.Count; i++)
+        {
+            var dampenedLevels = _levels.ToList();
+            dampenedLevels.RemoveAt(i);
+
+            if (IsSafe(dampenedLevels))
+                isSafeAfterDampening = true;
+        }
+
+        return isSafeAfterDampening;
+    }
 }
+
+public record Level(int Value);
--- AoC.UnitTests/AoC.2024/Day02Tests.cs
+++ AoC.UnitTests/AoC.2024/Day02Tests.cs
@@ -34,4 +34,32 @@ public class Day02Tests
 
         actual.Should().Be(321);
     }
+
+    [Fact(DisplayName = "2024 day 02 part 02 with test input")]
+    public void PartTwo_TestInput()
+    {
+        var input = new[]
+        {
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+        };
+
+        var actual = _sut.PartTwo(input);
+
+        actual.Should().Be(4);
+    }
+
+    [Fact(DisplayName = "2024 day 02 part 02 with actual input", Skip = "input file not included")]
+    public void PartTwo_ActualInput()
+    {
+        var input = FileReader.ReadAllLines(TestDataFile);
+
+        var actual = _sut.PartTwo(input);
+
+        actual.Should().Be(386);
+    }
 }
