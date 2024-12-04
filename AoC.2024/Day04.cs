f717a2bf30caddf6f945907357407107c0656dc9
spenserca <spenserca@gmail.com>
2024-12-04 06:32:03 -0600

    wip

---
+++ AoC.2024/Day04.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using AoC.Common;
+
+namespace AoC._2024;
+
+public class Day04: IDayPartOne
+{
+    public string Title => "--- Day 4: Ceres Search ---";
+    
+    public object PartOne(string[] input)
+    {
+        var rowMatches = input.Select(i => new WordSearchLine(i).GetNumberOfWordInstances()).Sum();
+        var columnMatches = BuildColumns(input).Select(i => new WordSearchLine(i).GetNumberOfWordInstances()).Sum();
+        var downwardDiagonalMatches = BuildDownwardDiagonals(input).Select(i => new WordSearchLine(i).GetNumberOfWordInstances()).Sum();
+        var upwardDiagonalMatches = BuildUpwardDiagonals(input).Select(i => new WordSearchLine(i).GetNumberOfWordInstances()).Sum();
+
+        return rowMatches + columnMatches + downwardDiagonalMatches + upwardDiagonalMatches;
+    }
+
+    private IEnumerable<string> BuildUpwardDiagonals(string[] input)
+    {
+        return [];
+    }
+
+    private IEnumerable<string> BuildDownwardDiagonals(string[] input)
+    {
+        return [];
+    }
+
+    private IEnumerable<string> BuildColumns(string[] input)
+    {
+        var columns = new List<string>();
+        foreach (var row in input)
+        {
+            var columnBuilder = new StringBuilder();
+            foreach (var v in row)
+            {
+                columnBuilder.Append(v);
+            }
+            columns.Add(columnBuilder.ToString());
+            columnBuilder.Clear();
+        }
+
+        return columns;
+    }
+}
+
+public class Grid(string[] input)
+{
+    private readonly IEnumerable<string> Rows = input;
+    private readonly IEnumerable<string> Columns = BuildColumns(input);
+    private readonly IEnumerable<string> DownwardDiagonals = BuildUpwardDiagonals(input);
+    private readonly IEnumerable<string> UpwardDiagonals = BuildDownwardDiagonals(input);
+    
+    private static IEnumerable<string> BuildColumns(string[] input)
+    {
+        var columns = new List<string>();
+        foreach (var row in input)
+        {
+            var columnBuilder = new StringBuilder();
+            foreach (var v in row)
+            {
+                columnBuilder.Append(v);
+            }
+            columns.Add(columnBuilder.ToString());
+            columnBuilder.Clear();
+        }
+
+        return columns;
+    }
+    
+    private static IEnumerable<string> BuildUpwardDiagonals(string[] input)
+    {
+        return [];
+    }
+
+    private static IEnumerable<string> BuildDownwardDiagonals(string[] input)
+    {
+        return [];
+    }
+}
+
+public class WordSearchLine(string value)
+{
+    private const string RegexToSearchFor = @"(XMAS)|(SAMX)";
+
+    public int GetNumberOfWordInstances()
+    {
+        return Regex.Matches(value, RegexToSearchFor).Count;
+    }
+}
---
+++ AoC.UnitTests/AoC.2024/Day04Tests.cs
@@ -0,0 +1,31 @@
+﻿using AoC._2024;
+
+namespace AoC.UnitTests.AoC._2024;
+
+public class Day04Tests
+{
+    private readonly Day04 _sut = new();
+    private const string TestDataFile = @"AoC.2024/Data/Day04.txt";
+
+    [Fact(DisplayName = "2024 day 04 part 01 with test input")]
+    public void PartOne_TestInput()
+    {
+        var input = new[]
+        {
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        };
+
+        var actual = _sut.PartOne(input);
+
+        actual.Should().Be(18);
+    }
+}
