13ed4ab1a823fcff075996873476c9b8b77f6c21
Spenser Andreassen <spenserca@gmail.com>
2024-12-05 08:13:12 -0600

    refactor tests to theories

--- AoC.UnitTests/AoC.Common/LineSegmentTests.cs
+++ AoC.UnitTests/AoC.Common/LineSegmentTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AoC.Common;
 using FluentAssertions.Execution;
 
@@ -6,98 +7,23 @@ namespace AoC.UnitTests.AoC.Common;
 
 public class LineSegmentTests
 {
-    [Fact(DisplayName =
-        "when the line is vertical with increasing slope, then the points on the line can be calculated")]
-    public void IncreasingVerticalLine()
+    [Theory(DisplayName = "the points on the line can be calculated")]
+    // vertical ascending
+    [InlineData(3, 5, 3, 8, new[] { 3, 3, 3, 3 }, new[] { 5, 6, 7, 8 })]
+    // vertical descending
+    [InlineData(3, 8, 3, 5, new[] { 3, 3, 3, 3 }, new[] { 8, 7, 6, 5 })]
+    // horizontal ascending
+    [InlineData(5, 3, 8, 3, new[] { 5, 6, 7, 8 }, new[] { 3, 3, 3, 3 })]
+    // horizontal descending
+    [InlineData(8, 3, 5, 3, new[] { 8, 7, 6, 5 }, new[] { 3, 3, 3, 3 })]
+    public void VerticalTests(int startX, int startY, int endX, int endY, int[] expectedX, int[] expectedY)
     {
-        var start = new Coordinate(3, 5);
-        var end = new Coordinate(3, 8);
+        var start = new Coordinate(startX, startY);
+        var end = new Coordinate(endX, endY);
         var line = new LineSegment(start, end);
-
-        var expected = new List<Coordinate>()
-        {
-            new(3, 5),
-            new(3, 6),
-            new(3, 7),
-            new(3, 8),
-        };
-
-        var actual = line.Points;
-
-        using (new AssertionScope())
-        {
-            actual.Should().HaveCount(expected.Count);
-            actual.Should().BeEquivalentTo(expected);
-        }
-    }
-    
-    [Fact(DisplayName =
-        "when the line is vertical with decreasing slope, then the points on the line can be calculated")]
-    public void DecreasingVerticalLine()
-    {
-        var start = new Coordinate(3, 8);
-        var end = new Coordinate(3, 5);
-        var line = new LineSegment(start, end);
-
-        var expected = new List<Coordinate>()
-        {
-            new(3, 8),
-            new(3, 7),
-            new(3, 6),
-            new(3, 5),
-        };
-
-        var actual = line.Points;
-
-        using (new AssertionScope())
-        {
-            actual.Should().HaveCount(expected.Count);
-            actual.Should().BeEquivalentTo(expected);
-        }
-    }
-    
-    [Fact(DisplayName =
-        "when the line is horizontal with increasing slope, then the points on the line can be calculated")]
-    public void IncreasingHorizontalLine()
-    {
-        var start = new Coordinate(5, 3);
-        var end = new Coordinate(8, 3);
-        var line = new LineSegment(start, end);
-
-        var expected = new List<Coordinate>()
-        {
-            new(5, 3),
-            new(6, 3),
-            new(7, 3),
-            new(8, 3),
-        };
-
-        var actual = line.Points;
-
-        using (new AssertionScope())
-        {
-            actual.Should().HaveCount(expected.Count);
-            actual.Should().BeEquivalentTo(expected);
-        }
-    }
-    
-    [Fact(DisplayName =
-        "when the line is horizontal with decreasing slope, then the points on the line can be calculated")]
-    public void DecreasingHorizontalLine()
-    {
-        var start = new Coordinate(5, 3);
-        var end = new Coordinate(8, 3);
-        var line = new LineSegment(start, end);
-        // TODO: add test cases crossing the origin, y-axis and x-axis
-
-        var expected = new List<Coordinate>()
-        {
-            new(8, 3),
-            new(7, 3),
-            new(6, 3),
-            new(5, 3),
-        };
-
+        
+        var expected = expectedX.Select((x, i) => new Coordinate(x, expectedY[i])).ToList();
+        
         var actual = line.Points;
 
         using (new AssertionScope())
