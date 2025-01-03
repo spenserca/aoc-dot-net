34017516ea1c13716c3aaa8e994f90d31f5c37f5
Spenser Andreassen <spenserca@gmail.com>
2025-01-03 08:33:50 -0600

    can get coordinate values in a direction

--- AoC.Common/Grid.cs
+++ AoC.Common/Grid.cs
@@ -21,10 +21,40 @@ public class Grid
         }
     }
 
+    public GridCoordinate? GetByValue(string value)
+    {
+        return Coordinates.FirstOrDefault(c => c.Value == value);
+    }
+
+    public GridCoordinate? GetValueInDirection(GridCoordinate current, Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up: return GetValueAtLocation(current.X, current.Y - 1);
+            case Direction.Down: return GetValueAtLocation(current.X, current.Y + 1);
+            case Direction.Left: return GetValueAtLocation(current.X - 1, current.Y);
+            case Direction.Right: return GetValueAtLocation(current.X + 1, current.Y);
+            default: return null;
+        }
+    }
+
+    private GridCoordinate? GetValueAtLocation(int x, int y)
+    {
+        return Coordinates.FirstOrDefault(c => c.X == x && c.Y == y);
+    }
+
     public override string ToString()
     {
         return string.Join(Environment.NewLine, _input);
     }
 }
 
-record GridCoordinate(int X, int Y, string Value);
+public record GridCoordinate(int X, int Y, string Value);
+
+public enum Direction
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3
+}
--- AoC.UnitTests/AoC.Common/GridTests.cs
+++ AoC.UnitTests/AoC.Common/GridTests.cs
@@ -1,5 +1,6 @@
 using System;
 using AoC.Common;
+using FluentAssertions.Execution;
 using Xunit.Abstractions;
 
 namespace AoC.UnitTests.AoC.Common;
@@ -22,11 +23,101 @@ public class GridTests(ITestOutputHelper logger)
             "#.........",
             "......#..."
         };
-        var expected = $"....#.....{Environment.NewLine}.........#{Environment.NewLine}..........{Environment.NewLine}..#.......{Environment.NewLine}.......#..{Environment.NewLine}..........{Environment.NewLine}.#..^.....{Environment.NewLine}........#.{Environment.NewLine}#.........{Environment.NewLine}......#...";
+        var expected =
+            $"....#.....{Environment.NewLine}.........#{Environment.NewLine}..........{Environment.NewLine}..#.......{Environment.NewLine}.......#..{Environment.NewLine}..........{Environment.NewLine}.#..^.....{Environment.NewLine}........#.{Environment.NewLine}#.........{Environment.NewLine}......#...";
 
         var actual = new Grid(input).ToString();
-        
+
         actual.Should().Be(expected);
         logger.WriteLine(actual);
+    }
+
+    [Fact(DisplayName = "can get the coordinate with a specific value")]
+    public void GetByValue()
+    {
+        var input = new[]
+        {
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#..."
+        };
+
+        var actual = new Grid(input).GetByValue("^");
+
+        using (new AssertionScope())
+        {
+            actual.Should().NotBeNull();
+            actual!.X.Should().Be(4);
+            actual.Y.Should().Be(6);
+            actual.Value.Should().Be("^");
+        }
+    }
+
+    [Theory(DisplayName = "can get the coordinates around the current coordinate when they have a value")]
+    [InlineData(4, 1, ".", Direction.Up, 4 , 0, "#")]
+    [InlineData(8, 6, ".", Direction.Down, 8 , 7, "#")]
+    [InlineData(5, 6, ".", Direction.Left, 4 , 6, "^")]
+    [InlineData(8, 1, ".", Direction.Right, 9 , 1, "#")]
+    public void GetCoodinatesAroundWithValues(int currentX, int currentY, string currentValue, Direction direction, int nextX, int nextY, string nextValue)
+    {
+        var input = new[]
+        {
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#..."
+        };
+
+        var actual = new Grid(input).GetValueInDirection(new GridCoordinate(currentX, currentY, currentValue), direction);
+
+        using (new AssertionScope())
+        {
+            actual.Should().NotBeNull();
+            actual!.X.Should().Be(nextX);
+            actual.Y.Should().Be(nextY);
+            actual.Value.Should().Be(nextValue);
+        }
+    }
+    
+    [Theory(DisplayName = "can get the coordinates around the current coordinate when they don't have a value")]
+    [InlineData(4, 0, ".", Direction.Up)]
+    [InlineData(8, 9, ".", Direction.Down)]
+    [InlineData(0, 6, ".", Direction.Left)]
+    [InlineData(9, 1, ".", Direction.Right)]
+    public void GetCoordinatesAroundWithoutValues(int currentX, int currentY, string currentValue, Direction direction)
+    {
+        var input = new[]
+        {
+            "....#.....",
+            ".........#",
+            "..........",
+            "..#.......",
+            ".......#..",
+            "..........",
+            ".#..^.....",
+            "........#.",
+            "#.........",
+            "......#..."
+        };
+
+        var actual = new Grid(input).GetValueInDirection(new GridCoordinate(currentX, currentY, currentValue), direction);
+
+        using (new AssertionScope())
+        {
+            actual.Should().BeNull();
+        }
     }
 }
